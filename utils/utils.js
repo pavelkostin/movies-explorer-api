669814cbffd85308638d6ed18767665e813d3fa6
pavelkostin <pasha.kostin@yandex.ru>
2022-01-13 00:07:53 +0300

    роуты, модели, контроллеры

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,6 @@
 const allowedCors = {
   origin: [
-    /* 'http://api.movie-kpa.nomoredomains.rocks/', */
+    'http://api.movie-kpa.nomoredomains.rocks/',
     'http://localhost:3000',
     'https://web.postman.co',
   ],
