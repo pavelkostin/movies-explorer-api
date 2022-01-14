ece3bd83bc6a211541f3f1a59bce56b7d03a5237
pavelkostin <pasha.kostin@yandex.ru>
2022-01-15 00:52:24 +0300

    адрес базы данных берётся из process.env

--- app.js
+++ app.js
@@ -13,10 +13,14 @@ const routes = require('./routes');
 
 const port = 3000;
 
-/* console.log(randomString); */
+const { NODE_ENV, DATABASE_URL } = process.env;
 
 // подключаем монго
-mongoose.connect('mongodb://localhost:27017/moviesdb', {
+/* mongoose.connect('mongodb://localhost:27017/moviesdb', {
+  useNewUrlParser: true,
+}); */
+
+mongoose.connect(NODE_ENV === 'production' ? DATABASE_URL : 'mongodb://localhost:27017/moviesdb', {
   useNewUrlParser: true,
 });
 
