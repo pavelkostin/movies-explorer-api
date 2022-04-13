c2b46a34a9539b647784e17d331e944772a4519e
pavelkostin <pasha.kostin@yandex.ru>
2022-04-13 15:20:41 +0300

    cors

--- utils/utils.js
+++ utils/utils.js
@@ -1,11 +1,11 @@
 const allowedCors = {
   origin: [
-    'http://kpa-movie-explorer-front.nomoredomains.work/',
+    'https://kpa-movie-explorer-front.nomoredomains.work/',
     /* 'https://api.movie-kpa.nomoredomains.rocks/', */
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://localhost:3002',
-    'http://localhost:3003',
+    'https://localhost:3000',
+    'https://localhost:3001',
+    'https://localhost:3002',
+    'https://localhost:3003',
     'https://web.postman.co',
   ],
   credentials: true,
